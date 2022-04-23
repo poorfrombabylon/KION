7f63a5f0b572b6af505a57b0d1399b0346f30b01
poorfrombabylon <al06maz2002@gmail.com>
2022-04-24 01:31:40 +0300

    feat: domains and events are added

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module KION
 
 go 1.18
+
+require github.com/google/uuid v1.3.0
--- service/domain/model.go
+++ service/domain/model.go
@@ -1,1 +1,29 @@
 package domain
+
+import (
+	"github.com/google/uuid"
+	"time"
+)
+
+type Model struct {
+	videoID   uuid.UUID
+	userID    uuid.UUID
+	eventType Event
+	eventTime time.Time
+}
+
+func (m Model) GetVideoID() uuid.UUID {
+	return m.videoID
+}
+
+func (m Model) GetUserID() uuid.UUID {
+	return m.userID
+}
+
+func (m Model) GetEvent() Event {
+	return m.eventType
+}
+
+func (m Model) GetEventTime() time.Time {
+	return m.eventTime
+}
