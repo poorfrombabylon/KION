422b68614b64f42b6b0a303f4930ee14051bf951
Nikita Ratashnyuk <nikitarat1@gmail.com>
2022-05-09 19:03:14 +0300

    moved initialization to main

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,13 @@
 package main
 
 import (
+	"context"
+	"log"
+	"time"
+
 	"net/http"
+
+	"github.com/ClickHouse/clickhouse-go/v2"
 )
 
 var UrlHello = []byte(`
@@ -17,9 +23,21 @@ var UrlHello = []byte(`
 	</body>
 `)
 
+const createTableQuery = `
+	CREATE TABLE IF NOT EXISTS KION
+	(
+		VideoId String,
+		UserId String,
+		EventType String,
+		EventTime DateTime
+	)
+
+	ENGINE = ReplacingMergeTree()
+	ORDER BY EventTime;
+`
+
 func handle228(w http.ResponseWriter, r *http.Request) {
 	w.Write(UrlHello)
-	db()
 	name := r.FormValue("userName")
 	if name != "" {
 		w.Write([]byte("Hello " + name))
@@ -27,6 +45,32 @@ func handle228(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+	var (
+		ctx       = context.Background()
+		conn, err = clickhouse.Open(&clickhouse.Options{
+			Addr: []string{"localhost:9000"},
+			Auth: clickhouse.Auth{
+				Database: "default",
+				Username: "default",
+				Password: "",
+			},
+			DialTimeout:     time.Second,
+			MaxOpenConns:    10,
+			MaxIdleConns:    5,
+			ConnMaxLifetime: time.Hour,
+		})
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = conn.Ping(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+	if err := conn.Exec(ctx, createTableQuery); err != nil {
+		log.Fatal(err)
+	}
+
 	http.HandleFunc("/", handle228)
 
 	http.HandleFunc("/hello", func(writer http.ResponseWriter, request *http.Request) {
